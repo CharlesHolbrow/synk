b88d0dd5b46b14cf6db393bc70f3dc70d10ddb65
Charles Holbrow <charlesholbrow@gmail.com>
2017-08-21 16:59:12 -0400

    documentation

--- ObjectConstructor.go
+++ ObjectConstructor.go
@@ -1,5 +1,9 @@
 package synk
 
-// ObjectConstructor is any function that can create an object from a type key and a
-// byte slice. If error is nil, the Object should have been created successfully.
+// ObjectConstructor is any function that can create an object from a type key
+// and a byte slice. If error is nil, the Object should have been created
+// successfully.
+//
+// You will need to write an ObjectConstructor for your synk server, so that the
+// server knows how to deserialize your objects based on their type keys
 type ObjectConstructor func(typeKey string, bytes []byte) (Object, error)
