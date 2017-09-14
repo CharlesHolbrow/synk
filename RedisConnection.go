424c337a662b361594538dc9374858c07fe94fbe
Charles Holbrow <charlesholbrow@gmail.com>
2017-09-14 14:30:47 -0400

    Deprecating GetID in favor of Random IDs with NewID()
    closes #6

--- RedisConnection.go
+++ RedisConnection.go
@@ -1,7 +1,6 @@
 package synk
 
 import (
-	"fmt"
 	"log"
 	"strconv"
 	"time"
@@ -34,31 +33,8 @@ func NewConnection(redisAddr string) *RedisConnection {
 	return arc
 }
 
-// GetID returns an ID string from redis. Each call incrememnts a counter,
-// meaning that this will never return the same value for any prefix twice. It
-// is intended to be used with typeKeys to generate character IDs.
-func (arc *RedisConnection) GetID(counterKey string) (string, error) {
-	conn := arc.Pool.Get()
-	defer conn.Close()
-	return getID(counterKey, conn)
-}
-
-// NewObjectID gets an ID from redis, and sets that on the new object.
-// We may eventully want to replace this with randomly generated IDs
-func (arc *RedisConnection) NewObjectID(o Object) error {
-	conn := arc.Pool.Get()
-	defer conn.Close()
-	if o.GetID() == "" {
-		if id, err := getID(o.TypeKey(), conn); err == nil {
-			o.SetID(id)
-		} else {
-			return fmt.Errorf("arc.NewObjectID failed to get ID: %s", err)
-		}
-	}
-	return nil
-}
-
-// GetID is a helper for retrieving unique ID for objects. The Typical use
+// GetID is a helper for retrieving unique ID for objects.
+// This is DEPRECATED in favor of RandomIDs.
 func getID(counterKey string, conn redis.Conn) (string, error) {
 	r, err := redis.Int(conn.Do("INCR", "count:"+counterKey))
 	if err != nil {
